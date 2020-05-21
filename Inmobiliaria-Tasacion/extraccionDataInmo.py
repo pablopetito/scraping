cd66f341d50fbf14bdaed854ce3306afda95daca
unknown <pablopetito@gmail.com>
2020-05-21 19:17:48 -0300

    Seleccionar inmuebles de un pais

--- Inmobiliaria-Tasacion/extraccionDataInmo.py
+++ Inmobiliaria-Tasacion/extraccionDataInmo.py
@@ -11,19 +11,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import datetime 
 import time
+import sys
+import re
 
 #paquetes propios
 
 #funciones
 def textoSolo(texto):
-    import re
     patron = re.compile('\s')
-    return patron.split(texto) 
+    palabra = patron.split(texto)
+    patron2 = '[A-Z][a-z]+'
+    matcher = re.findall(patron2, texto)
+    palabrafinal=""
+    for tx in matcher:
+        palabrafinal = palabrafinal + " " + tx 
+    return palabrafinal  
 
 def numeroSolo(numero):
-    import re
-    patron = re.compile('[\(\)]')
-    return patron.split(numero) 
+    patron = '[0-9]+'
+    numero2=re.findall(patron, numero)
+    final=""
+    for nro in numero2:
+        final = final + nro
+    return final 
 
 #hora Inicial
 hrsInicial=datetime.datetime.now()
@@ -57,13 +67,13 @@ element.send_keys(Keys.ENTER)
 time.sleep(2)
 
 # --- Presiona boton Cancelar de una ventana emergente
-element = driver.find_element_by_xpath("//*[@id='suggestAlertPopup']/div/form/button[2]").click()
+#element = driver.find_element_by_xpath("//*[@id='suggestAlertPopup']/div/form/button[2]").click()
 
 # --- tiempo de espera 
 time.sleep(3)
 
 # --- Borra todos los filtros de la busquedad
-element = driver.find_element_by_xpath("/html/body/main/div[1]/sidebar/div[2]/div/button").click()
+element = driver.find_element_by_xpath("/html/body/main/div[2]/sidebar/div[1]/div/button").click()
 
 # --- Recolectar link PAISES 
 #//*[@id="argentina"]
@@ -71,8 +81,9 @@ element = driver.find_element_by_xpath("/html/body/main/div[1]/sidebar/div[2]/di
 #/html/body/main/div[1]/sidebar/div[4]/div/div[1]/div/div[2]/ul[1]/li[1]/a
 
 time.sleep(2)
-pais = driver.find_element_by_xpath('//*[@id="argentina"]').text
-paises = driver.find_element_by_xpath('//*[@id="locationFilter"]/ul[1]/li[1]/a').text
+paisArgentina = driver.find_element_by_xpath('//*[@id="argentina"]').text
+paisUruguay = driver.find_element_by_xpath('//*[@id="uruguay"]').text
+
 paises2 = driver.find_element_by_xpath('//*[@id="locationFilter"]/ul[1]').text
 allPaises = driver.find_elements_by_xpath('//*[@id="locationFilter"]/ul[1]/li')
 todosPaises = []
@@ -80,29 +91,33 @@ for paisesNom in allPaises:
     todosPaises.append(paisesNom.text)
 
 time.sleep(2)
-element = driver.find_element_by_xpath("/html/body/main/div[1]/sidebar/div[4]/div/div[1]/div/div[2]/ul[1]/li[1]/a").click()
-print("pais")
-print(pais)
-print("paises")
-print(paises)
-print("paises2")
-print(paises2)
-print("paises de array")
+#element = driver.find_element_by_xpath("/html/body/main/div[1]/sidebar/div[4]/div/div[1]/div/div[2]/ul[1]/li[1]/a").click()
+
+
+paises_nom = []
+paises_avisos = []
 
 for nombre in todosPaises:
-    pais_cant = textoSolo(nombre)
-    pais = pais_cant[0]
-#    cantidadAvisos = numeroSolo(pais_cant[1])
-    print("* " + pais + " avisos: " + cantidadAvisos[1])
+    paises_nom.append(textoSolo(nombre))   
+    paises_avisos.append(numeroSolo(nombre))
+
+for i in range(len(paises_nom)):
+    print(paises_nom[i] + ": " + paises_avisos[i])
+
+
+#HACER CLICK EN UN PAIS *****
+pais = paises_nom[1].strip().lower()
+element = driver.find_element_by_xpath('//*[@id=\'{}\']'.format(pais)).click()
+#element = driver.find_element_by_xpath('//option[@value=\'{}\']'.format(State)).click()
+
+sys.exit()
+
+    #print("* " + pais + " avisos: " + cantidadAvisos )
     
 #print("pais 3: " + todosPaises[2])
 
 #pais = textoSolo(todosPaises[0])
 #cantidadAvisos = numeroSolo(pais[1])
-
-print(pais[0])
-print(cantidadAvisos[1])
-
 
 #titulos=driver.find_element_by_xpath("/html/body/section[3]/section/article[2]/div/div/div[{}]/h2/a/span".format(i))
 #subtitulos=driver.find_element_by_xpath("/html/body/section[3]/section/article[2]/div/div/div[{}]/ul/li[{}]".format(i,j))
